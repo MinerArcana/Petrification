bde4c1bc29b1df67cceab2f8136d61367b7ff8eb
Skylar Sommers <ssommers@criticalmix.com>
2017-08-23 14:09:24 -0400

    Set up mod file

---
+++ src/main/java/com/minerarcana/petrification/Petrification.java
@@ -0,0 +1,37 @@
+package com.minerarcana.petrification;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+import static com.minerarcana.petrification.Petrification.MODID;
+import static com.minerarcana.petrification.Petrification.NAME;
+import static com.minerarcana.petrification.Petrification.VERSION;
+
+@Mod(modid = MODID, name = NAME, version = VERSION)
+public class Petrification {
+    public static final String MODID = "petrification";
+    public static final String NAME = "Petrification";
+    public static final String VERSION = "@VERSION@";
+
+    @Instance
+    public static Petrification instance;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+}
