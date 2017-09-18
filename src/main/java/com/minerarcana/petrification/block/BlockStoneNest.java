41a507e171e0bd49337119913166cb9e0c5ee55e
SkySom <skylar.som@gmail.com>
2017-09-17 22:38:30 -0400

    Finished up Stone Nest

--- src/main/java/com/minerarcana/petrification/block/BlockStoneNest.java
+++ src/main/java/com/minerarcana/petrification/block/BlockStoneNest.java
@@ -9,13 +9,21 @@ import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 
 import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.Random;
 
 public class BlockStoneNest extends Block {
     private final static PropertyBool EGG = PropertyBool.create("egg");
+    public static final AxisAlignedBB FULL_BLOCK_AABB = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.5D, 1.0D);
     public final static String NAME = "stone_nest";
 
     public BlockStoneNest() {
@@ -26,6 +34,44 @@ public class BlockStoneNest extends Block {
         this.setHardness(1.5F);
         this.setResistance(10.0F);
         this.setSoundType(SoundType.STONE);
+        this.setTickRandomly(true);
+        this.fullBlock = false;
+    }
+
+    @Override
+    @ParametersAreNonnullByDefault
+    public void randomTick(World world, BlockPos pos, IBlockState state, Random random) {
+        if (state.getValue(EGG) && world.provider.getMoonPhase(world.getWorldTime()) == 0) {
+            spawnCockatrice(world, pos);
+        }
+    }
+
+    private void spawnCockatrice(World world, BlockPos pos) {
+
+    }
+
+    @Override
+    public boolean canPlaceBlockAt(World world, @Nonnull BlockPos pos) {
+        BlockPos downPos = pos.down();
+        return world.getBlockState(downPos).isSideSolid(world, pos, EnumFacing.UP);
+    }
+
+    @Override
+    public boolean doesSideBlockRendering(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing face) {
+        return false;
+    }
+
+    @Override
+    @Nonnull
+    @SuppressWarnings("deprecation")
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+        return FULL_BLOCK_AABB;
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public boolean isOpaqueCube(IBlockState state) {
+        return false;
     }
 
     @Override
@@ -46,8 +92,7 @@ public class BlockStoneNest extends Block {
     }
 
     @Override
-    public void getSubBlocks(CreativeTabs itemIn, NonNullList<ItemStack> items)
-    {
+    public void getSubBlocks(CreativeTabs itemIn, NonNullList<ItemStack> items) {
         items.add(new ItemStack(this, 1, 0));
         items.add(new ItemStack(this, 1, 1));
     }
